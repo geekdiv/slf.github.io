fa9da98d8c24dcdd9519e73d5ad7f9209c191129
Divyesh Kanzariya <kanzariyadivyesh@gmail.com>
2021-01-17 18:54:58 +0530

    js changes
    js changes

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@
  	easing: 'slide'
  });
 
+const el = document.querySelector('img');
+const observer = lozad(el); // passing a `NodeList` (e.g. `document.querySelectorAll()`) is also valid
+observer.observe();
+
 (function($) {
 
 	"use strict";
